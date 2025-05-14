4e49c8af32e74e4e39a8ebfcc29197ca6c0df55e
Benjamin Schweizer <bs103@hdm-stuttgart.de>
2025-05-14 03:54:51 +0200

    Allow to inject a TextSpanBuilder which allows a user to use a custom TextSpan

--- lib/controller/social_text_editing_controller.dart
+++ lib/controller/social_text_editing_controller.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_social_textfield/flutter_social_textfield.dart';
 
-
+typedef TextSpan TextSpanBuilder(
+    {String? text, TextStyle? style, GestureRecognizer? recognizer});
 
 ///An improved [TextEditingController] for using with any widget that accepts [TextEditingController].
 ///It uses [SocialTextSpanBuilder] for rendering the content.
@@ -19,9 +21,22 @@ import 'package:flutter_social_textfield/flutter_social_textfield.dart';
 ///
 ///There is also a helper function that can replaces range with the given value. In order to change cursor context, cursor moves to next word after replacement.
 ///
-class SocialTextEditingController extends TextEditingController{
+class SocialTextEditingController extends TextEditingController {
+  final TextSpanBuilder? textSpanBuilder;
 
-  StreamController<SocialContentDetection> _detectionStream = StreamController<SocialContentDetection>.broadcast();
+  StreamController<SocialContentDetection> _detectionStream =
+      StreamController<SocialContentDetection>.broadcast();
+
+  final Map<DetectedType, TextStyle> detectionTextStyles = Map();
+
+  final Map<DetectedType, RegExp> _regularExpressions = {
+    DetectedType.mention: atSignRegExp,
+    DetectedType.hashtag: hashTagRegExp,
+    DetectedType.url: urlRegex,
+    DetectedType.emoji: emojiRegex,
+  };
+
+  SocialTextEditingController({super.text, this.textSpanBuilder});
 
   @override
   void dispose() {
@@ -29,82 +44,88 @@ class SocialTextEditingController extends TextEditingController{
     super.dispose();
   }
 
-  final Map<DetectedType, TextStyle> detectionTextStyles = Map();
-
-  final Map<DetectedType, RegExp> _regularExpressions = {
-    DetectedType.mention:atSignRegExp,
-    DetectedType.hashtag:hashTagRegExp,
-    DetectedType.url:urlRegex,
-    DetectedType.emoji:emojiRegex,
-  };
-
-  StreamSubscription<SocialContentDetection> subscribeToDetection(Function(SocialContentDetection detected) listener){
+  StreamSubscription<SocialContentDetection> subscribeToDetection(
+      Function(SocialContentDetection detected) listener) {
     return _detectionStream.stream.listen(listener);
   }
 
-  void setTextStyle(DetectedType type, TextStyle style){
+  void setTextStyle(DetectedType type, TextStyle style) {
     detectionTextStyles[type] = style;
   }
 
-  void setRegexp(DetectedType type, RegExp regExp){
+  void setRegexp(DetectedType type, RegExp regExp) {
     _regularExpressions[type] = regExp;
   }
 
-  void replaceRange(String newValue, TextRange range){
-    // print("newValue: $newValue, range: $range: ${range.textInside(text)}");
+  void replaceRange(String newValue, TextRange range) {
     var newText = text.replaceRange(range.start, range.end, newValue);
-    var newRange = TextRange(start: range.start, end: range.start + newValue.length);
-    // print("Updated Range Content: [${newRange.textAfter(newText)}], text length: ${newText.length}, ${newRange.end}");
-    bool isAtTheEndOfText = (newRange.textAfter(newText) == " " && newRange.end == newText.length - 1);
-    if(isAtTheEndOfText){
+    var newRange =
+        TextRange(start: range.start, end: range.start + newValue.length);
+    bool isAtTheEndOfText = (newRange.textAfter(newText) == " " &&
+        newRange.end == newText.length - 1);
+    if (isAtTheEndOfText) {
       newText += " ";
     }
-    TextSelection newTextSelection = TextSelection(baseOffset: newRange.end + 1, extentOffset: newRange.end + 1);
+    TextSelection newTextSelection = TextSelection(
+        baseOffset: newRange.end + 1, extentOffset: newRange.end + 1);
     value = value.copyWith(text: newText, selection: newTextSelection);
   }
 
-  void _processNewValue(TextEditingValue newValue){
+  void _processNewValue(TextEditingValue newValue) {
     var currentPosition = newValue.selection.baseOffset;
-    if(currentPosition == -1){
+    if (currentPosition == -1) {
       currentPosition = 0;
     }
-    if(currentPosition >newValue.text.length){
+    if (currentPosition > newValue.text.length) {
       currentPosition = newValue.text.length - 1;
     }
-    var subString = newValue.text.substring(0,currentPosition);
+    var subString = newValue.text.substring(0, currentPosition);
 
     var lastPart = subString.split(" ").last.split("\n").last;
     var startIndex = currentPosition - lastPart.length;
-    var detectionContent = newValue.text.substring(startIndex).split(" ").first.split("\n").first;
-    _detectionStream.add(SocialContentDetection(getType(detectionContent), TextRange(start: startIndex, end: startIndex + detectionContent.length), detectionContent));
+    var detectionContent =
+        newValue.text.substring(startIndex).split(" ").first.split("\n").first;
+    _detectionStream.add(SocialContentDetection(
+        getType(detectionContent),
+        TextRange(start: startIndex, end: startIndex + detectionContent.length),
+        detectionContent));
   }
-  
-  DetectedType getType(String word){
-    return _regularExpressions.keys.firstWhere((type) => _regularExpressions[type]!.hasMatch(word),orElse: ()=>DetectedType.plain_text);
+
+  DetectedType getType(String word) {
+    return _regularExpressions.keys.firstWhere(
+        (type) => _regularExpressions[type]!.hasMatch(word),
+        orElse: () => DetectedType.plain_text);
   }
 
   @override
   set value(TextEditingValue newValue) {
-
-    if(newValue.selection.baseOffset >= newValue.text.length){
-      print("will add space");
-      newValue = newValue
-          .copyWith(
+    if (newValue.selection.baseOffset >= newValue.text.length) {
+      newValue = newValue.copyWith(
           text: newValue.text.trimRight() + " ",
-          selection: newValue.selection.copyWith(baseOffset: newValue.text.length, extentOffset: newValue.text.length));
+          selection: newValue.selection.copyWith(
+              baseOffset: newValue.text.length,
+              extentOffset: newValue.text.length));
     }
-    if(newValue.text == " "){
-      newValue = newValue
-          .copyWith(
+    if (newValue.text == " ") {
+      newValue = newValue.copyWith(
           text: "",
-          selection: newValue.selection.copyWith(baseOffset: 0, extentOffset: 0));
+          selection:
+              newValue.selection.copyWith(baseOffset: 0, extentOffset: 0));
     }
     _processNewValue(newValue);
     super.value = newValue;
   }
 
   @override
-  TextSpan buildTextSpan({required BuildContext context, TextStyle? style, required bool withComposing}) {
-    return SocialTextSpanBuilder(regularExpressions: _regularExpressions,defaultTextStyle: style,detectionTextStyles: detectionTextStyles).build(text);
+  TextSpan buildTextSpan(
+      {required BuildContext context,
+      TextStyle? style,
+      required bool withComposing}) {
+    return SocialTextSpanBuilder(
+      regularExpressions: _regularExpressions,
+      defaultTextStyle: style,
+      detectionTextStyles: detectionTextStyles,
+      textSpanBuilder: textSpanBuilder,
+    ).build(text);
   }
 }
